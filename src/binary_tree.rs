76a95da9a5b30155245ed1883bf9959013d8597b
zzeng <zzeng@fairtide.co>
2022-07-23 21:29:50 +0800

    feat: bst impl intoiter

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -1,7 +1,3 @@
-use std::cell::RefCell;
-use std::ops::{Deref, DerefMut};
-use std::rc::Rc;
-
 #[derive(Debug, PartialEq, PartialOrd)]
 pub struct Node<T> {
     pub value: T,
@@ -16,15 +12,47 @@ impl<T> Node<T> {
             value,
             count: 1,
             lchild: None,
-            rchild: None
+            rchild: None,
         }
     }
 }
 
-
 #[derive(Debug, PartialEq)]
 pub struct BST<T> {
     root: Option<Node<T>>,
+}
+
+pub struct BSTPreOrderIterator<'a, T> {
+    stack: Vec<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for BSTPreOrderIterator<'a, T> {
+    type Item = &'a Node<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.stack.is_empty() {
+            return None;
+        }
+        let cur = self.stack.pop().unwrap();
+        if cur.rchild.is_some() {
+            self.stack.push(cur.rchild.as_ref().unwrap());
+        }
+        if cur.lchild.is_some() {
+            self.stack.push(cur.lchild.as_ref().unwrap());
+        }
+        return Some(cur);
+    }
+}
+
+impl<'a, T> IntoIterator for &'a BST<T> {
+    type Item = &'a Node<T>;
+    type IntoIter = BSTPreOrderIterator<'a, T>;
+
+    fn into_iter(self) -> BSTPreOrderIterator<'a, T> {
+        BSTPreOrderIterator {
+            stack: vec![self.root().unwrap()],
+        }
+    }
 }
 
 impl<T> BST<T> {
@@ -44,6 +72,10 @@ impl<T> BST<T> {
             Some(ref mut node) => Some(node),
             None => None,
         }
+    }
+
+    pub fn preorder_iter(&self) -> self::BSTPreOrderIterator<T> {
+        self.into_iter()
     }
 
     pub fn insert(&mut self, node: Node<T>)
@@ -76,29 +108,28 @@ impl<T> BST<T> {
         }
     }
 
-    pub fn search(&self, value: T) -> Option<&Node<T>> 
+    pub fn search(&self, value: T) -> Option<&Node<T>>
     where
         T: PartialEq + PartialOrd,
     {
-        let mut cur_node: & Node<T> = self.root().unwrap();
+        let mut cur_node: &Node<T> = self.root().unwrap();
 
         loop {
             if cur_node.value < value {
                 match &cur_node.rchild {
                     Some(n) => cur_node = n.as_ref(),
-                    None => return None
+                    None => return None,
                 }
             } else if cur_node.value > value {
                 match &cur_node.lchild {
                     Some(n) => cur_node = n.as_ref(),
-                    None => return None
+                    None => return None,
                 }
             } else {
                 return Some(cur_node);
             }
-        } 
+        }
     }
-
 }
 
 #[cfg(test)]
@@ -125,29 +156,28 @@ mod tests {
 
     #[test]
     fn insert() {
-        let expected_bst = BST::new(
-            Node {
-                value: 1,
-                count: 1,
+        let expected_bst = BST::new(Node {
+            value: 1,
+            count: 1,
+            lchild: None,
+            rchild: Some(Box::new(Node {
+                value: 2,
+                count: 2,
                 lchild: None,
                 rchild: Some(Box::new(Node {
-                    value: 2,
-                    count: 2,
-                    lchild: None, 
-                    rchild: Some(Box::new(Node {
-                        value: 4,
+                    value: 4,
+                    count: 1,
+                    lchild: Some(Box::new(Node {
+                        value: 3,
                         count: 1,
-                        lchild: Some(Box::new(Node {
-                            value: 3,
-                            count: 1,
-                            lchild: None,
-                            rchild: None,
-                        })),
+                        lchild: None,
                         rchild: None,
                     })),
+                    rchild: None,
                 })),
-            });
-                    
+            })),
+        });
+
         let mut bst = BST::new(Node::new(1));
         bst.insert(Node::new(2));
         bst.insert(Node::new(4));
@@ -164,5 +194,39 @@ mod tests {
 
         assert_eq!(bst.search(2), Some(&Node::new(2)));
         assert_eq!(bst.search(3), None);
+    }
+
+    #[test]
+    fn preorder_iter() {
+        let tree = BST::new(Node {
+            value: 4,
+            count: 1,
+            lchild: Some(Box::new(Node {
+                value: 3,
+                count: 1,
+                lchild: None,
+                rchild: None,
+            })),
+            rchild: Some(Box::new(Node {
+                value: 6,
+                count: 2,
+                lchild: Some(Box::new(Node {
+                    value: 5,
+                    count: 1,
+                    lchild: None,
+                    rchild: None,
+                })),
+                rchild: Some(Box::new(Node {
+                    value: 7,
+                    count: 1,
+                    lchild: None,
+                    rchild: None,
+                })),
+            })),
+        });
+        let expected_vals = [4, 3, 6, 5, 7];
+        for (idx, node) in tree.preorder_iter().enumerate() {
+            assert_eq!(expected_vals[idx], node.value);
+        }
     }
 }
